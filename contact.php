0ef5082ecc364169d9253db423047bfd905a56dc
Tim <timevko@gmail.com>
2014-03-18 22:38:29 -0400

    small module in contact page

--- contact.php
+++ contact.php
@@ -15,6 +15,10 @@
                 <p>Need some front end consulting on your newest build? Shoot me an email and I'll get back to you as soon as possible.</p>
                 <p>Interested in getting your team up to date on the latest front end technologies? Let me know the time and place.</p>
                 <p>Find me on Twitter <a href="http://twitter.com/tevko" target="_blank">@Tevko</a>, Email me at timevko (at) gmail (dot) com, or <a href="#">take a look at my resume</a>. I'm also on <a href="http://codepen.io/tevko" target="_blank">Codepen</a> , <a href="http://github.com/tevko" target="_blank">Github</a>, and <a href="http://www.linkedin.com/pub/tim-evko/65/73b/7b4" target="_blank">Linkedin</a>.</p>
+                <div class="projects">
+                    <h2>Projects I Work on</h2>
+                    <div class="project-item practice"><p>Practice your skills!</p></div><div class="project-item resources"><p>Resources for front end web developers</p></div>
+                </div>
             </article>
         </section>
 <?php include("footer.php"); ?>
--- footer.php
+++ footer.php
@@ -17,9 +17,9 @@
             }(document, 'script'));
             </script>
         </footer>
-        <!--dont forget to remove!
+        <!--dont forget to remove!-->
     <script src='//192.168.1.3:3000/socket.io/socket.io.js'></script>
     <script>var ___socket___ = io.connect('http://192.168.1.3:3000');</script>
-    <script src='//192.168.1.3:3001/client/browser-sync-client.0.6.2.js'></script>-->
+    <script src='//192.168.1.3:3001/client/browser-sync-client.0.6.2.js'></script>
     </body>
 </html>
